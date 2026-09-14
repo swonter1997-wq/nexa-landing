a3c5c11a902020e416b574d62c0cee2ca75e8c8d
swonter1997-wq <swonter1997@gmail.com>
2026-09-14 07:24:50 +0300

    Add mobile menu animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const menuButton = document.querySelector(".menu-button");
 const nav = document.querySelector("header nav");
+const navLinks = document.querySelectorAll("header nav a");
 
 menuButton.addEventListener("click", function () {
     nav.classList.toggle("active");
+});
+
+navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+        nav.classList.remove("active");
+    });
 });
